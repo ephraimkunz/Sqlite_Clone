2ad08f485ab0f400b772c362d0c99fdc275eb5a9
Ephraim Kunz <ephraimkunz@me.com>
2017-11-17 18:12:48 -0700

    Stubs for VM

--- sqlite.c
+++ sqlite.c
@@ -3,7 +3,19 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define IN_BUFFER_SIZE 30
+typedef enum meta_command_result_t {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED
+} MetaCommandResult;
+
+typedef enum prepare_result_t {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED
+} PrepareResult;
+
+typedef enum prepared_type_t {
+    STATEMENT_INSERT, STATEMENT_SELECT
+} StatementType;
 
 typedef struct input_buffer_t {
     char* buffer;
@@ -11,9 +23,16 @@ typedef struct input_buffer_t {
     ssize_t input_len;
 } InputBuffer;
 
+typedef struct statement_t {
+    StatementType type;
+} Statement;
+
 void print_prompt();
 void read_input(InputBuffer* buffer);
+MetaCommandResult do_meta_command(InputBuffer* buffer);
 InputBuffer* new_input_buffer();
+PrepareResult prepare_statement(InputBuffer* buffer, Statement* statement);
+void execute_statement(Statement* statement);
 
 int main() {
     InputBuffer* input_buffer = new_input_buffer();
@@ -21,11 +40,62 @@ int main() {
         print_prompt();
         read_input(input_buffer);
 
-        if(strcmp(input_buffer->buffer, ".exit") == 0) {
-            exit(0);
-        } else {
-            printf("Unrecognized command [%s]\n", input_buffer->buffer);
+        if(input_buffer->buffer[0] == '.') { // Meta command
+            switch(do_meta_command(input_buffer)) {
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case META_COMMAND_UNRECOGNIZED:
+                printf("Unrecognized command [%s]\n", input_buffer->buffer);
+                continue;
+            }
         }
+
+        // Not a meta command
+        Statement statement;
+        switch(prepare_statement(input_buffer, &statement)) {
+        case PREPARE_SUCCESS:
+            break;
+        case PREPARE_UNRECOGNIZED:
+            printf("Unrecognized keyword at start of [%s]\n", input_buffer->buffer);
+            continue;
+        }
+
+        // We have a real Statement here
+        execute_statement(&statement);
+    }
+}
+
+MetaCommandResult do_meta_command(InputBuffer* buffer) {
+    if(strcmp(buffer->buffer, ".exit") == 0) {
+        exit(0);
+    } else {
+        return META_COMMAND_UNRECOGNIZED;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer* buffer, Statement* statement) {
+    if(strncmp(buffer->buffer, "insert", 6) == 0) { // Starts with 'insert'...followed by more
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+
+    if(strcmp(buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED;
+}
+
+// This is our VM
+void execute_statement(Statement* statement) {
+    switch(statement->type) {
+    case STATEMENT_INSERT:
+        printf("This is where we would do an insert\n");
+        break;
+    case STATEMENT_SELECT:
+        printf("This is where we would do a select\n");
+        break;
     }
 }
 

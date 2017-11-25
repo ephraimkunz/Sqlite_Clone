240413c4a19aa19b03772a47551533d8e627c9ab
Ephraim Kunz <ephraimkunz@me.com>
2017-11-25 09:48:34 -0700

    Add cursor abstraction

--- sqlite.c
+++ sqlite.c
@@ -63,6 +63,12 @@ typedef struct statement_t {
     Row row_to_insert;
 } Statement;
 
+typedef struct cursor_t {
+    Table* table;
+    uint32_t row_num;
+    bool end_of_table; // Indicates a position one past the last element.
+} Cursor;
+
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
 const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
@@ -81,7 +87,7 @@ PrepareResult prepare_statement(InputBuffer* buffer, Statement* statement);
 ExecuteResult execute_statement(Statement* statement, Table* table);
 void serialize_row(Row* source, void* destination);
 void deserialize_row(void* source, Row* destination);
-void* row_slot(Table* table, uint32_t row_num);
+void* cursor_value(Cursor* cursor);
 ExecuteResult execute_select(Statement* statement, Table* table);
 ExecuteResult execute_insert(Statement* statement, Table* table);
 Table* db_open(const char* filename);
@@ -90,6 +96,9 @@ Pager* pager_open(const char* filename);
 void* get_page(Pager* pager, uint32_t page_num);
 void db_close(Table* table);
 void pager_flush(Pager* pager, uint32_t page_num, uint32_t size);
+Cursor* table_start(Table* table);
+Cursor* table_end(Table* table);
+void cursor_advance(Cursor* cursor);
 
 int main(int argc, char* argv[]) {
     if(argc < 2) {
@@ -152,6 +161,24 @@ MetaCommandResult do_meta_command(InputBuffer* buffer, Table* table) {
     } else {
         return META_COMMAND_UNRECOGNIZED;
     }
+}
+
+Cursor* table_start(Table* table) {
+    Cursor* cursor = (Cursor*) malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = table->num_rows == 0;
+
+    return cursor;
+}
+
+Cursor* table_end(Table* table) {
+    Cursor* cursor = (Cursor*) malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+
+    return cursor;
 }
 
 PrepareResult prepare_insert(InputBuffer* buffer, Statement* statement) {
@@ -294,18 +321,26 @@ ExecuteResult execute_insert(Statement* statement, Table* table){
     }
 
     Row* row_to_insert = &(statement->row_to_insert);
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    Cursor* cursor = table_end(table);
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows++;
+
+    free(cursor);
 
     return EXECUTE_SUCCESS;
 }
 
 ExecuteResult execute_select(Statement* statement, Table* table) {
+    Cursor* cursor = table_start(table);
     Row row;
-    for(uint32_t i = 0; i < table->num_rows; i++) {
-        deserialize_row(row_slot(table, i), &row);
+
+    while(!(cursor->end_of_table)) {
+        deserialize_row(cursor_value(cursor), &row);
         print_row(&row);
+        cursor_advance(cursor);
     }
+
+    free(cursor);
 
     return EXECUTE_SUCCESS;
 }
@@ -326,12 +361,18 @@ void deserialize_row(void* source, Row* destination) {
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
-void* row_slot(Table* table, uint32_t row_num) {
+void* cursor_value(Cursor* cursor) {
+    uint32_t row_num = cursor->row_num;
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = get_page(table->pager, page_num);
+    void* page = get_page(cursor->table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE; // Which row index in page
     uint32_t byte_offset = row_offset * ROW_SIZE; // Which byte in the page does this actually map to
     return page + byte_offset;
+}
+
+void cursor_advance(Cursor* cursor) {
+    cursor->row_num ++;
+    cursor->end_of_table = cursor->row_num >= cursor->table->num_rows;
 }
 
 void print_prompt() {
